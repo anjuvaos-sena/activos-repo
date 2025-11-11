1ea9ab2d4677bfd510e8f70bb80957a5a5b2fde5
anjuvaos <ajvalenciaosorio@gmail.com>
2025-11-11 08:59:27 -0500

    estaticos

--- pr_activos/settings.py
+++ pr_activos/settings.py
@@ -123,7 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static/"
+]
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
